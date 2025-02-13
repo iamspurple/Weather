be861f74148503f6cf8d4afafd66999473afffa1
Sabina Gasanova <sabina21gasanova@yandex.ru>
2025-02-13 19:07:53 +0300

    code fixed

--- src/config.js
+++ src/config.js
@@ -1,13 +1,23 @@
 export const API_URL = "https://api.openweathermap.org";
 export const API_ID = "bd2faa91511838506ebf94d5d39215ab";
+import Clouds from "/icons/clouds.png";
+import Snow from "/icons/snow.png";
+import Drizzle from "/icons/rain.png";
+import Thunderstorm from "/icons/thunderstorm.png";
+import Clear from "/icons/sun.png";
+import Fog from "/icons/fog.png";
+import Mist from "/icons/fog.png";
+import Haze from "/icons/fog.png";
+import Rain from "/icons/rain.png";
+
 export const weatherIcons = {
-  Clouds: "../../icons/clouds.png",
-  Snow: "../../icons/snow.png",
-  Drizzle: "../../icons/rain.png",
-  Rain: "../../icons/rain.png",
-  Thunderstorm: "../../icons/thunderstorm.png",
-  Clear: "../../icons/sun.png",
-  Fog: "../../icons/fog.png",
-  Mist: "../../icons/fog.png",
-  Haze: "../../icons/fog.png",
+  Clouds,
+  Snow,
+  Drizzle,
+  Rain,
+  Thunderstorm,
+  Clear,
+  Fog,
+  Mist,
+  Haze,
 };
